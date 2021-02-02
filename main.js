696e219d3b813f8a2ed6bb8057fe074b65cb8b26
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2021-02-02 15:40:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status="";
+objects=[];
 function preload(){
     video=createVideo("video.mp4");
 }
@@ -12,6 +13,29 @@ function setup(){
 
 function draw(){
     image(video,0,0,500,400);
+    if(status !=""){
+        objectDetector.detect(video,getResult);
+       for(i=0;i<objects.length;i++){
+       document.getElementById("status").innerHTML="Status:Objects Detected";
+       document.getElementById("number_of_objects").innerHTML="Number of objects detected:"+objects.length;
+       
+       fill("gold");
+       percent=floor(objects[i].confidence*100);
+       text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+       textSize(20);
+       noFill();
+       stroke("gold");
+       strokeWeight(3);
+       rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+       }
+    }
+}function getResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects=results;
+    }
 }
 
 function start(){
